e2cbc1d5037698f27aace1486b6a4b4186445ffa
Chakakrin Smachitsuriya <powerhere123@gmail.com>
2023-01-10 20:32:01 +0700

    edit dark

--- src/components/WorkShop.vue
+++ src/components/WorkShop.vue
@@ -8,20 +8,20 @@
         aria-labelledby="stats-tab"
       >
         <dl
-          class="grid max-w-screen-xl grid-cols-2 gap-8 p-4 mx-auto text-gray-900 sm:grid-cols-3 xl:grid-cols-4 dark:text-white sm:p-8"
+          class="grid max-w-screen-xl grid-cols-2 gap-8 p-4 mx-auto text-gray-900 sm:grid-cols-3 xl:grid-cols-4  sm:p-8"
         >
           <div
-            class="flex flex-col items-start md:items-center justify-start md:justify-center p-2 hover:shadow-lg hover:shadow-green-400 dark:hover:shadow-neutral-300 transition ease-in hover:bg-emerald-100 dark:hover:bg-slate-800"
+            class="flex flex-col items-start md:items-center justify-start md:justify-center p-2 hover:shadow-lg hover:shadow-green-400 transition ease-in hover:bg-emerald-100 "
           >
             <dt class="mb-2 text-2xl font-extrabold">FIGMA</dt>
           </div>
           <div
-            class="flex flex-col items-start md:items-center justify-start md:justify-center p-2 hover:shadow-lg hover:shadow-green-400 dark:hover:shadow-neutral-300 transition ease-in hover:bg-emerald-100 dark:hover:bg-slate-800"
+            class="flex flex-col items-start md:items-center justify-start md:justify-center p-2 hover:shadow-lg hover:shadow-green-400 transition ease-in hover:bg-emerald-100 "
           >
             <dt class="mb-2 text-2xl font-extrabold">Visual Studio Code</dt>
           </div>
           <div
-            class="flex flex-col items-start md:items-center justify-start md:justify-center p-2 hover:shadow-lg hover:shadow-green-400 dark:hover:shadow-neutral-300 transition ease-in hover:bg-emerald-100 dark:hover:bg-slate-800"
+            class="flex flex-col items-start md:items-center justify-start md:justify-center p-2 hover:shadow-lg hover:shadow-green-400 transition ease-in hover:bg-emerald-100 "
           >
             <dt class="mb-2 text-2xl font-extrabold">Microsoft Office</dt>
           </div>
